8e6e6e2d7fc43f939e3564b2005715b716a60192
Colt Ossoff <coltossoff@gmail.com>
2024-05-02 13:31:56 -0500

    commented out entity, config

---
+++ src/app/server/entities/entry.entity.ts
@@ -0,0 +1,16 @@
+// import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+// @Entity({tableName: 'entries'})
+// export class EntryEntity {
+//     @PrimaryKey()
+//     _id!: number;
+
+//     @Property()
+//     title!: string;
+
+//     @Property()
+//     descr!: string;
+
+//     @Property()
+//     createdAt: Date = new Date();
+// }
